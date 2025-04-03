2de3e433b32cc64c45c562b6b2e531e2a2f6ccc2
TheUsernameIsNotLong <hmike3030@gmail.com>
2025-04-04 00:38:15 +0100

    created db

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
+import os
 from datetime import datetime
 from flask import Flask, render_template
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 moment = Moment(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship("User", backref="role", lazy="dynamic")
+    
+    def __repr__(self):
+        return f"<Role {self.name!r}>"
+    
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+    
+    def __repr__(self):
+        return f"<User {self.username!r}>"
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
 
 
 @app.errorhandler(404)
@@ -25,3 +59,4 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
