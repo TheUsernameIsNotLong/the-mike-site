f672e023a1950ec2323af1a9e8d4f67d68473948
TheUsernameIsNotLong <hmike3030@gmail.com>
2025-04-14 21:46:09 +0100

    login verification

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import URLSafeTimedSerializer as Serializer
+from flask import current_app
 from flask_login import UserMixin
 from . import db, login_manager
 
@@ -20,6 +22,7 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(32), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
     password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
     
     @property
     def password(self):
@@ -31,6 +34,22 @@ class User(UserMixin, db.Model):
     
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def generate_confirmation_token(self):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'confirm': self.id})
+    
+    def confirm(self, token, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token, expiration)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
     
     def __repr__(self):
         return f"<User {self.username!r}>"
