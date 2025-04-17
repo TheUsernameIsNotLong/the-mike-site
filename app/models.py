3771e237e078c91b294aae80c89915e85b59f869
TheUsernameIsNotLong <hmike3030@gmail.com>
2025-04-17 21:08:03 +0100

    password reset implementation

--- app/models.py
+++ app/models.py
@@ -50,7 +50,25 @@ class User(UserMixin, db.Model):
         self.confirmed = True
         db.session.add(self)
         return True
-    
+
+    def generate_reset_token(self):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'reset': self.id})
+
+    @staticmethod
+    def reset_password(token, new_password, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token, expiration)
+        except:
+            return False
+        user = User.query.get(data.get('reset'))
+        if user is None:
+            return False
+        user.password = new_password
+        db.session.add(user)
+        return True
+
     def __repr__(self):
         return f"<User {self.username!r}>"
 
