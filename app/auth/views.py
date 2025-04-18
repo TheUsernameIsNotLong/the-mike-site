676eb1964c5751c8687c48429616af1d4dbc2e4a
TheUsernameIsNotLong <hmike3030@gmail.com>
2025-04-18 15:56:01 +0100

    basic profile info

--- app/auth/views.py
+++ app/auth/views.py
@@ -9,12 +9,13 @@ from .forms import LoginForm, RegistrationForm, ChangePasswordForm, PasswordRese
 
 @auth.before_app_request
 def before_request():
-    if current_user.is_authenticated \
-            and not current_user.confirmed \
-            and request.endpoint \
-            and request.blueprint != 'auth' \
-            and request.endpoint != 'static':
-        return redirect(url_for('auth.unconfirmed'))
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
+                and request.endpoint \
+                and request.blueprint != 'auth' \
+                and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
 
 
 @auth.route('/unconfirmed')
@@ -103,6 +104,7 @@ def change_password():
         else:
             flash('Invalid password.', "error")
     return render_template("auth/change_password.html", form=form)
+
 
 @auth.route('/reset', methods=['GET', 'POST'])
 def password_reset_request():
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import URLSafeTimedSerializer as Serializer
 from flask import current_app
@@ -70,6 +71,10 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.now(timezone.utc))
+    last_seen = db.Column(db.DateTime(), default=datetime.now(timezone.utc))
     
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -129,6 +134,10 @@ class User(UserMixin, db.Model):
 
     def is_administrator(self):
         return self.can(Permission.ADMIN)
+
+    def ping(self):
+        self.last_seen = datetime.now(timezone.utc)
+        db.session.add(self)
 
     def __repr__(self):
         return f"<User {self.username!r}>"
